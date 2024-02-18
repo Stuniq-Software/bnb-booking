68c02517f67990130843c1b3672efb3781059e72
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-02-18 13:32:26 +0530

    Currency to lower case fix

--- util/payment.py
+++ util/payment.py
@@ -8,7 +8,7 @@ stripe.api_key = os.getenv("STRIPE_API_KEY")
 def create_payment_id(amount: float, currency: str, description: str, booking_id: str) -> str:
     intent = stripe.PaymentIntent.create(
         amount=int(amount*100),
-        currency=currency,
+        currency=currency.lower(),
         description=description,
         metadata={"booking_id", booking_id},
         payment_method_types=["card"],
