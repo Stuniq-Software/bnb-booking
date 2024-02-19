987bd3ed8a84f1491970fe881a5a3a53e1b79cff
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-02-19 08:06:18 +0530

    Fixed metadata in Stripe API

--- util/payment.py
+++ util/payment.py
@@ -6,13 +6,15 @@ stripe.api_key = os.getenv("STRIPE_API_KEY")
 
 
 def create_payment_id(amount: float, currency: str, description: str, booking_id: str) -> str:
+    print(f"Creating payment intent for {amount} {currency} for booking {booking_id}")
     intent = stripe.PaymentIntent.create(
         amount=int(amount*100),
         currency=currency.lower(),
         description=description,
-        metadata={"booking_id", booking_id},
-        payment_method_types=["card"],
-        confirm=True
+        automatic_payment_methods={"enabled": True}
     )
+
+    stripe.PaymentIntent.modify(intent["id"], metadata={"booking_id": booking_id})
+
     return intent['id']
 
